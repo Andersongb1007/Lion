3cbfe16ba9a4420070a502df962b1f53ddb10eea
Anderson Gonzalez <72115143+Andersongb1007@users.noreply.github.com>
2022-02-08 18:14:05 -0400

    Arreglos parte 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,3 @@
-/**
- * WEBSITE: https://themefisher.com
- * TWITTER: https://twitter.com/themefisher
- * FACEBOOK: https://www.facebook.com/themefisher
- * GITHUB: https://github.com/themefisher/
- */
 
 (function ($) {
   'use strict';
@@ -175,6 +169,6 @@
 
 /*SCROLL*/
 var scroll = new SmoothScroll('a[href*="#"]', {
-  speed: 1000,
+  speed: 500,
   speedAsDuration: true,
 });
